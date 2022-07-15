a021aee6bf018b716ab2f3aeeaaa05cbb99b9583
Timothy LaRock <timothylarock@gmail.com>
2022-07-15 09:27:09 +0100

    Removed echos.

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -11,10 +11,10 @@ for ensemble in "cdg" "odg" "hypa" "rw-uw" "rw-w"
 do
 	for k in 2 3
 	do
-		echo julia --project=.. --threads $threads motif_sampling.jl -i ../data/flights/2020/coupons_2020_q1.ngram -e $ensemble -k $k -f -w -1 -o ../results/motifs/ &
-		echo julia --project=.. --threads $threads motif_sampling.jl -i ../data/flights/2020/coupons_2020_q2.ngram -e $ensemble -k $k -f -w -1 -o ../results/motifs/
+		julia --project=.. --threads $threads motif_sampling.jl -i ../data/flights/2020/coupons_2020_q1.ngram -e $ensemble -k $k -f -w -1 -o ../results/motifs/ &
+		julia --project=.. --threads $threads motif_sampling.jl -i ../data/flights/2020/coupons_2020_q2.ngram -e $ensemble -k $k -f -w -1 -o ../results/motifs/
 
-		echo julia --project=.. --threads $threads motif_sampling.jl -i ../data/wikispeedia/wikispeedia_finished_all.ngram -e $ensemble -k $k -w -1 -o ../results/motifs/ &
-		echo julia --project=.. --threads $threads motif_sampling.jl -i ../data/wikispeedia/wikispeedia_unfinished_all.ngram -e $ensemble -k $k -w -1 -o ../results/motifs/
+		julia --project=.. --threads $threads motif_sampling.jl -i ../data/wikispeedia/wikispeedia_finished_all.ngram -e $ensemble -k $k -w -1 -o ../results/motifs/ &
+		julia --project=.. --threads $threads motif_sampling.jl -i ../data/wikispeedia/wikispeedia_unfinished_all.ngram -e $ensemble -k $k -w -1 -o ../results/motifs/
 	done
 done
